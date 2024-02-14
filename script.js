175b129c74e4e18dbdf004bf06c5d884d634105b
saurabhm93 <160041154+saurabhm93@users.noreply.github.com>
2024-02-14 23:16:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // You can add logic here to handle form submission, e.g., sending data to a server.
+
+    alert('Your message has been sent!');
+});
